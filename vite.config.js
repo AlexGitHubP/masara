77de2cadef9f3748bf7d33bc5a645c1401e0541f
Alexandru Popa <alex@jobful.ro>
2025-03-16 10:53:16 +0200

    Update composer and vite.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/js/app.js'],
+            input: [
+                'resources/js/app.js',
+                'resources/js/validators.js',
+                'resources/js/addProduct.js',
+                'resources/js/shop.js',
+                'resources/js/cart.js',
+                'resources/sass/global.scss'
+            ],
             refresh: true,
         }),
     ],
